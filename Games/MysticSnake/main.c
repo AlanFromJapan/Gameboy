113b2db016779666e6d1666a8772cce8ba2a82a2
AlanFromJapan <Alan@BigOne01>
2020-04-01 18:47:33 +0900

    Now can write strings on demand on BG (just a test)

--- Games/MysticSnake/main.c
+++ Games/MysticSnake/main.c
@@ -159,8 +159,78 @@ void showTitle(){
             break;
         }
     }
+    
 }
 
+unsigned char dynmap[18*20];
+
+void writetext (UINT8 px, UINT8 py, char* msg){
+
+    for (UINT8 i =0; msg[i] != 0; i++){
+        UINT8 c = msg[i];
+
+        if (c >='a' && c <= 'z'){
+            dynmap[py * 20 + px] = (UINT8)c - (UINT8)'A' - ('a' - 'A') + (UINT8)TILE_LETTER_1;
+        }
+        else 
+            if (c >='A' && c <= 'Z'){
+                dynmap[py * 20 + px] = (UINT8)c - (UINT8)'A' + (UINT8)TILE_LETTER_1;
+            }
+            else {
+                switch(c){
+                    case ' ':
+                        dynmap[py * 20 + px] = TILE_EMPTY;
+                        break;
+                    case '.':
+                        dynmap[py * 20 + px] = TILE_LETTER_DOT;
+                        break;
+                    case '?':
+                        dynmap[py * 20 + px] = TILE_LETTER_QUESTION;
+                        break;
+                    case '!':
+                        dynmap[py * 20 + px] = TILE_LETTER_EXCL;
+                        break;
+                }
+            }
+
+        px++;
+        if (px >= 20){
+            px = 0;
+            py ++;
+        }
+    }
+
+}
+
+void test_text(){
+    for (UINT16 i = 0 ; i < 18*20; i++){
+        dynmap[i] = TILE_EMPTY;
+    }
+
+    dynmap[20] = TILE_LETTER_1;
+    dynmap[21] = TILE_LETTER_20;
+    dynmap[22] = TILE_NUMBER_2;
+
+    writetext(4, 5, "Alexandre TROP MIGNON CHOUPINOU!!");
+    writetext(4, 10, "ABCD");
+
+    bgx = 0;
+    bgy = 0;
+
+    set_bkg_tiles(0, 0, 20, 18, dynmap);
+
+    SHOW_BKG;
+
+    delay(500);
+
+    while(1) {
+        lastJoypad = joypad();
+        if(lastJoypad & J_START) {
+            break;
+        }
+    }
+
+}
 
 /**
  * MAIN method
@@ -174,6 +244,9 @@ void main() {
 
     //Shows the title screen
     showTitle();
+
+    //TEST
+    test_text();
 
     set_bkg_tiles(bgx, bgy, Map_Intro_WIDTH, Map_Intro_HEIGHT, Map_Intro);
     currentMap = Map_Intro;
