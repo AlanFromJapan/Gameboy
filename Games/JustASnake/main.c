2d5200fd1ad36be8f38d99a4d03fbc5d42edeaec
AlanFromJapan <Alan@BigOne01>
2020-04-24 22:39:11 +0900

    basics in progress, no collision detection yet, some display bugs left

--- Games/JustASnake/main.c
+++ Games/JustASnake/main.c
@@ -15,20 +15,235 @@
 #include "Map_About.h"
 #include "Map_Arene.h"
 
+
+#define SPEED_START 250
+#define SPEED_MIN   60
+UINT8 speed = SPEED_START;
+
+#define SNAKE_SIZE  32
+
+//The snake body [tile num 0 = head][x,y in TILES not px]
+UINT8 snake[SNAKE_SIZE][2];
+UINT8 snakeLen = 1;
+#define HEAD        snake[0]
+#define TAIL        snake[snakeLen-1]
+
+#define GETX(v)     (v[0])
+#define GETY(v)     (v[1])
+
+#define SETX(v, x)  v[0] = x;
+#define SETY(v, y)  v[1] = y;
+
+#define ITEMS_SIZE  10
+
+UINT8 arena[Map_Arene_WIDTH * Map_Arene_HEIGHT];
+
+UINT16 score = 1234;
+
+/**
+ * Shows title screen
+ * 
+ */
+inline void showTitle(){
+    set_bkg_tiles(0, 0, Map_SplashScreen_WIDTH, Map_SplashScreen_HEIGHT, Map_SplashScreen);
+    SHOW_BKG;
+
+    while(1) {
+        if(joypad() & J_START) {
+            break;
+        }
+        delay(10);
+    }
+
+    //debouncing
+    delay(200);
+}
+
+
+/**
+ * Shows arena screen
+ * 
+ */
+void showArena(){
+    set_bkg_tiles(0, 0, Map_Arene_WIDTH, Map_Arene_HEIGHT, Map_Arene);
+    SHOW_BKG;
+
+}
+
+/**
+ * Set a background tile to the tile in parameter
+ */
+void putTile(UINT8 tile, UINT8 x, UINT8 y){
+    //NOT inline so tile is a byte in the call stack so you can ref its address
+    set_bkg_tiles(x, y, 1, 1, &tile);
+
+}
+
+
+void placeItem(UINT8 tile, UINT8 x, UINT8 y){
+    arena[y * Map_Arene_WIDTH + x] = tile;
+    putTile(tile, x ,y);
+
+}
+
+
+
+void updateScore(){
+    UINT8 v = 0;
+
+    v = score % 10;
+    putTile(TILE_NUMBER_BLACK_1 + v, Map_Arene_WIDTH-2, Map_Arene_HEIGHT-1);
+    v = (score / 10) % 10;
+    putTile(TILE_NUMBER_BLACK_1 + v, Map_Arene_WIDTH-3, Map_Arene_HEIGHT-1);
+    v = (score / 100) % 10;
+    putTile(TILE_NUMBER_BLACK_1 + v, Map_Arene_WIDTH-4, Map_Arene_HEIGHT-1);
+    v = (score / 1000) % 10;
+    putTile(TILE_NUMBER_BLACK_1 + v, Map_Arene_WIDTH-5, Map_Arene_HEIGHT-1);
+}
+
+void moveTo(UINT8 x, UINT8 y){
+
+    //erase tail
+    putTile(TILE_EMPTY, GETX(TAIL), GETY(TAIL));
+
+    //slide every items 
+    for(UINT8 i =snakeLen; snakeLen > 1 && i > 0; i--){
+        SETX (snake[i-1], GETX(snake[i-2]));
+        SETY (snake[i-1], GETY(snake[i-2]));
+    }
+
+    UINT8 nextTile = arena[y * Map_Arene_WIDTH + x];
+    switch (nextTile) {
+        case TILE_BONBON:
+            //copy the snake content to one more cell backward
+            for(UINT8 i =snakeLen; snakeLen > 1 && i > 0; i--){
+                SETX (snake[i], GETX(snake[i-1]));
+                SETY (snake[i], GETY(snake[i-1]));
+            }
+
+            //grow
+            snakeLen ++;
+
+            //remove the bonbon
+            arena[y * Map_Arene_WIDTH + x] = 0;
+            
+            //speedup
+            speed = speed - (speed/8);
+
+            //get points
+            score += 100;
+            updateScore();
+            break;
+        
+    }
+
+
+    SETY(HEAD, y);
+    SETX(HEAD, x);
+}
+
+
+/**
+ * Voids all the arena cells
+ */
+void initArena(){
+    for (UINT8 i = 0; i < Map_Arene_WIDTH; i++)    
+        for (UINT8 j = 0; j < Map_Arene_HEIGHT; j++)    
+            arena[i*j] = 0;
+}
+
+/**
+ * Vertical blank interrupt: where we "draw" memory while screen is not updated
+ */
+void vblint(){
+
+    for (UINT8 i = 0; i < snakeLen; i++){
+        if (i == 0)
+            putTile(TILE_SNAKE_HEAD, GETX(snake[i]), GETY(snake[i]));
+        else
+            putTile(TILE_SNAKE_BODY, GETX(snake[i]), GETY(snake[i]));
+    }
+}
+
+/*****************************************************************************************
+ * 
+ * Main()
+ * 
+ */
 void main() {
     SPRITES_8x8;
 
     set_sprite_data(0, my_lib01_COUNT, my_lib01);
     set_bkg_data(0, my_lib01_COUNT, my_lib01);
-    set_bkg_tiles(0, 0, Map_SplashScreen_WIDTH, Map_SplashScreen_HEIGHT, Map_SplashScreen);
 
     SHOW_BKG;
+
+    showTitle();
+
+    initArena();
+    showArena();
     SHOW_SPRITES;
 
-    wait_vbl_done();
+    wait_vbl_done();    
 
+    //init the snake
+    SETX(HEAD, 10);
+    SETY(HEAD, 9);
+    snakeLen = 1;
+
+
+    placeItem(TILE_BONBON, 4, 4);
+    placeItem(TILE_BONBON, 4, 5);
+    placeItem(TILE_BONBON, 4, 6);
+    placeItem(TILE_BONBON, 4, 7);
+
+
+    updateScore();
+
+    //branch interrup handler for VBlank
+    add_VBL(vblint);
+
+    INT8 dx = -1;
+    INT8 dy = 0;
     while (1) {
-        wait_vbl_done();
+        UINT8 t = 0;
+        while (t < speed) {
+            UINT8 lastJoypad = joypad();
+            if(lastJoypad & J_RIGHT ) {
+                dx=1;
+                dy=0;
+            }
+            if(lastJoypad & J_LEFT ) {
+                dx=-1;
+                dy=0;
+            }
+            if(lastJoypad & J_UP ) {
+                dy=-1;
+                dx=0;
+            }
+            if(lastJoypad & J_DOWN) {
+                dy=1;
+                dx=0;
+            }
+
+            t += 10;
+            delay(10);
+        }
+
+        UINT8 newx = GETX(HEAD) + dx;
+        UINT8 newy = GETY(HEAD) + dy;
+        if (dx != 0 && newx >0 && newx < Map_Arene_WIDTH-1){
+            moveTo(newx, GETY(HEAD));
+        }
+        else {
+            //TODO HIT A WALL
+        }
+        if (dy != 0 && newy > 0 && newy < Map_Arene_HEIGHT -2){
+            moveTo(GETX(HEAD), newy);
+        }
+        else {
+            //TODO HIT A WALL
+        }
 
     }
 }
