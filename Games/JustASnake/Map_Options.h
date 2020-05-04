8f1fd4560e8cd675bd53fc3e261b848fdb93f578
AlanFromJapan <Alan@WinWM>
2020-05-04 05:07:48 -0700

    Added option screen and maps export as png

---
+++ Games/JustASnake/Map_Options.h
@@ -0,0 +1,12 @@
+
+#ifndef __Map_Options_H__
+#define __Map_Options_H__
+
+#define Map_Options_WIDTH   20
+#define Map_Options_HEIGHT  18
+
+extern const unsigned char Map_Options[];
+
+#endif  //__Map_Options_H__
+
+
