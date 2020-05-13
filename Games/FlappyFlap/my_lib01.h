f62ca74a49b7668ed3166a73c40726c6150fd48a
Alan@Vm <alanfromjapan@gmail.com>
2020-05-13 23:56:46 +0900

    Added basic tiles for FlappyFlap

---
+++ Games/FlappyFlap/my_lib01.h
@@ -0,0 +1,94 @@
+
+#ifndef __my_lib01_H__
+#define __my_lib01_H__
+
+#define my_lib01_COUNT   79
+
+
+#define TILE_TUYAUX_1   0
+#define TILE_TUYAUX_2   1
+#define TILE_TUYAUX_3   2
+#define TILE_TUYAUX_4   3
+#define TILE_TUYAUX_5   4
+#define TILE_TUYAUX_6   5
+#define TILE_QBLOCK_NW   6
+#define TILE_QBLOCK_SW   7
+#define TILE_QBLOCK_NE   8
+#define TILE_QBLOCK_SE   9
+#define TILE_COIN_NW   10
+#define TILE_COIN_SW   11
+#define TILE_COIN_NE   12
+#define TILE_COIN_SE   13
+#define TILE_BELL_NW   14
+#define TILE_BELL_SW   15
+#define TILE_BELL_NE   16
+#define TILE_BELL_SE   17
+#define TILE_EMPTY   18
+#define TILE_NUMBER_1   19
+#define TILE_NUMBER_2   20
+#define TILE_NUMBER_3   21
+#define TILE_NUMBER_4   22
+#define TILE_NUMBER_5   23
+#define TILE_NUMBER_6   24
+#define TILE_NUMBER_7   25
+#define TILE_NUMBER_8   26
+#define TILE_NUMBER_9   27
+#define TILE_NUMBER_10   28
+#define TILE_MOUCHE_NW   29
+#define TILE_MOUCHE_SW   30
+#define TILE_MOUCHE_NE   31
+#define TILE_MOUCHE_SE   32
+#define TILE_BLOCK_GENTIL_NW   33
+#define TILE_BLOCK_GENTIL_SW   34
+#define TILE_BLOCK_GENTIL_NE   35
+#define TILE_BLOCK_GENTIL_SE   36
+#define TILE_BLOCK_MECHANT_NW   37
+#define TILE_BLOCK_MECHANT_SW   38
+#define TILE_BLOCK_MECHANT_NE   39
+#define TILE_BLOCK_MECHANT_SE   40
+#define TILE_BLOCK_ARROW_RIGHT_NW   41
+#define TILE_BLOCK_ARROW_RIGHT_SW   42
+#define TILE_BLOCK_ARROW_RIGHT_NE   43
+#define TILE_BLOCK_ARROW_RIGHT_SE   44
+#define TILE_BLOCK_SMALL_EYES_NW   45
+#define TILE_BLOCK_SMALL_EYES_SW   46
+#define TILE_BLOCK_SMALL_EYES_NE   47
+#define TILE_BLOCK_SMALL_EYES_SE   48
+#define TILE_CHAMPIGNON_NW   49
+#define TILE_CHAMPIGNON_SW   50
+#define TILE_CHAMPIGNON_NE   51
+#define TILE_CHAMPIGNON_SE   52
+#define TILE_GHOST_A_NW   53
+#define TILE_GHOST_A_SW   54
+#define TILE_GHOST_A_NE   55
+#define TILE_GHOST_A_SE   56
+#define TILE_GHOST_B_NW   57
+#define TILE_GHOST_B_SW   58
+#define TILE_GHOST_B_NE   59
+#define TILE_GHOST_B_SE   60
+#define TILE_MINE_NW   61
+#define TILE_MINE_SW   62
+#define TILE_MINE_NE   63
+#define TILE_MINE_SE   64
+#define TILE_GROUND1   65
+#define TILE_GROUND2   66
+#define TILE_MOSS_NW   67
+#define TILE_MOSS_SW   68
+#define TILE_MOSS_NE   69
+#define TILE_MOSS_SE   70
+#define TILE_BG_STAR_NW   71
+#define TILE_BG_STAR_NE   72
+#define TILE_BG_STAR_SW   73
+#define TILE_BG_STAR_SE   74
+#define TILE_BG_FLOWER_NW   75
+#define TILE_BG_FLOWER_SW   76
+#define TILE_BG_FLOWER_NE   77
+#define TILE_BG_FLOWER_SE   78
+
+
+//declared as const to save compile and execution time/space
+extern const unsigned char my_lib01[]; 
+
+#endif //__my_lib01_H__
+
+
