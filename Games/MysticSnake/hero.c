53135ae8caebafe26fac89dbed6a8529ed015f09
Alan@Swift1 <alanfromjapan@gmail.com>
2022-08-01 23:15:50 +0900

    Comment and tests clearnup in Hero.c. Changed the order of items in the gbxml file to hopefuly improve the code (Simpler checks): - All the collisions are at the begining (list check becomes a simple less than check) - Groupped the hero and alternate sprite together for simpler code logic too - Added hearts for later, and zero too

--- Games/MysticSnake/hero.c
+++ Games/MysticSnake/hero.c
@@ -13,7 +13,6 @@ struct hero hero;
 UINT8 const COLLISION_TILE[] = {
 TILE_SAPIN_NW, TILE_SAPIN_NE, TILE_SAPIN_SW, TILE_SAPIN_SE, 
 TILE_PALMTREE_NW, TILE_PALMTREE_NE, TILE_PALMTREE_SW, TILE_PALMTREE_SE,
-TILE_PALMTREE_NW, TILE_PALMTREE_NE, TILE_PALMTREE_SW, TILE_PALMTREE_SE,
 TILE_ROCK_NW, TILE_ROCK_NE, TILE_ROCK_SW, TILE_ROCK_SE, 
 TILE_COLUMN_NW, TILE_COLUMN_NE, TILE_COLUMN_SW, TILE_COLUMN_SE, 
 TILE_TOWER_1, TILE_TOWER_2, TILE_TOWER_3, TILE_TOWER_4, TILE_TOWER_5
@@ -38,19 +37,16 @@ TILE_STAIRS_DOWN_NW, TILE_STAIRS_DOWN_NE, TILE_STAIRS_DOWN_SW, TILE_STAIRS_DOWN_
  * Check if collision, return 0 if no collision and edits the delta x & y, 1 if collision, 2 if transition
  */
 UINT8 checkCollision (UINT8 x, UINT8 y, INT8 *dx, INT8 *dy){
-    //x,y are in the bottom-middle of the Sprite
-    // UINT8 nx = GET_MAP_X(x, *dx);
-    // UINT8 ny = GET_MAP_Y(y, *dy);
+    ///////////////////////////////////////////////
+    //
+    // X, Y are the **BOTTOM RIGHT** point of the LEFT sprite  (therefore **middle bottom** of a 16x16 sprite)!
+    //
+    ///////////////////////////////////////////////
+
 
     UINT8 tileX = 0, tileY = 0, tileX2 = 0, tileY2 = 0;
     UINT8 result = MOVE_CHECK_OK;
 
-    // tileX = GET_BG_TILE(bgx + x  + *dx ,   bgy + y -1);
-    // tileY = GET_BG_TILE(bgx + x - 8 - 1,                    bgy + y + *dy -1);
-    // tileX = GET_BG_TILE( GET_MAP_X(x, *dx),     GET_MAP_Y(y, 0));
-    // tileY = GET_BG_TILE( GET_MAP_X(x, 0),       GET_MAP_Y(y, *dy));
-
-    //X, Y are the **BOTTOM RIGHT** point of the sprite  (therefore __middle bottom__ of a 16x16 sprite)!
     
     //Zelda style (?) detection
     // to respect the feeling of depth, feet can't get onto walls, but head a little, and L/R just 1 pixel
