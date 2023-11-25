dbd769f3973a51de35bb77d37fba9f0fe2537cc1
AlanFromJapan <alanfromjapan@gmail.com>
2023-11-25 23:16:34 +0900

    Init rolling dice "game" for RPG

---
+++ Games/RPGDice/main.c
@@ -0,0 +1,95 @@
+
+#include <gb/gb.h>
+#include <gb/drawing.h>
+
+#include "rolling_dice.h"
+#include "my_lib01.h"
+#include "Map_Screen1.h"
+
+
+
+
+
+/*
+ ***********************************************************************************************
+ * MAIN method
+ ***********************************************************************************************
+ */
+void main() {
+    UINT8 X,Y ;
+    INT8 dx, dy, SPRITE_FRAME;
+
+    SPRITES_8x16;
+
+    set_sprite_data(0, TILE_COUNT, my_tiles);
+    set_bkg_data(0, my_lib01_COUNT, my_lib01);
+
+    set_bkg_tiles(0, 0, Map_Screen1_WIDTH, Map_Screen1_HEIGHT, Map_Screen1);
+    
+    SHOW_BKG;
+    SHOW_SPRITES;
+
+
+    X = 100;
+    Y = 100;
+    dx = 0;
+    dy = 0;
+
+    SPRITE_FRAME=0;
+
+    set_sprite_tile(0, SPRITE_1);
+    set_sprite_tile(1, SPRITE_1+2);
+    move_sprite(0, 20, 20);
+    move_sprite(1, 20+8, 20);
+
+
+    move_sprite(2, X, Y);
+    move_sprite(3, X+8, Y);
+
+
+    wait_vbl_done();
+
+    while(1) {
+
+        wait_vbl_done();
+        if (joypad() & J_UP){
+            dy--;
+        }
+        if (joypad() & J_DOWN){
+            dy++;
+        }
+        if (joypad() & J_LEFT){
+            dx--;
+        }
+        if (joypad() & J_RIGHT){
+            dx++;
+        }
+
+
+        X = X + dx;
+        Y = Y + dy;
+
+        //TODO: fix this it's copy pasted from other project that was for 16x16 sprites
+        //X is the MIDDLE of the 16x16 sprite
+        if (X <= (8+8) || X >= ((20-1) * 8 -8) )
+            dx=-dx;
+
+        //Y is the BOTTOM of the 16x16 sprite
+        if (Y <= (8+16) || Y >= ((18-1) *8) )
+            dy=-dy;
+
+        move_sprite(2, X, Y);
+        move_sprite(3, X+8, Y);
+
+        //rotate sprite
+        SPRITE_FRAME = (SPRITE_FRAME + 1) % 3;
+        UINT8 s = 4 + 4 * SPRITE_FRAME;
+
+            
+        set_sprite_tile(2, s);
+        set_sprite_tile(3, s+2);
+        delay(150);
+    }
+}
+
+
---
+++ Games/RPGDice/rolling_dice.h
@@ -0,0 +1,19 @@
+
+#ifndef __my_tiles_H__
+#define __my_tiles_H__
+
+#define TILE_COUNT		16
+
+#define SPRITE_1		0
+#define SPRITE_2		4
+#define SPRITE_3		8
+#define SPRITE_4		12
+
+
+
+//declared as const to save compile and execution time/space
+extern const unsigned char my_tiles[]; 
+
+#endif //__my_tiles_h__
+
+
